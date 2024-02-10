81e603c011ea752adae34233dd7f619d6c3f082b
Bruno Dambroski <bruno3414.dambroski@gmail.com>
2024-02-10 11:03:18 -0400

    pequena alteração no banner

--- src/components/HeroPerfil/styled.ts
+++ src/components/HeroPerfil/styled.ts
@@ -2,11 +2,30 @@ import styled from "styled-components";
 import fundoPerfil from '../../Assets/Images/fundo_perfil.png'
 
 export const Container = styled.div`
+  position: relative;
   background-image: url(${fundoPerfil});
   background-repeat: no-repeat;
   background-size: cover;
   padding-top: 24px;
   padding-bottom: 32px;
+  width: 100%;
+  height: 280px;
+
+  &::after {
+    position: absolute;
+    background-color: #000;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    content: '';
+    opacity: 0.56;
+  }
+
+  .container {
+    z-index: 1;
+    position: relative;
+  }
 `
 export const Title1 = styled.h2`
   color: #fff;
